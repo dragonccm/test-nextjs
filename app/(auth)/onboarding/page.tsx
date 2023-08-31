4680e5742197a4675a6910d54d21d90b6d03580b
dragonccm <121989450+dragonccm@users.noreply.github.com>
2023-08-31 14:15:47 +0700

    g

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+async function page(){
+    return(
+        <main>
+            <h1 className="head-text">onboarding</h1>
+        </main>
+    )
+}
+
+export default page;
--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-
-module.exports = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'img-cdn.2game.vn',
-                port: '',
-                pathname: '/my-bucket/**',
-            },
-        ],
-    },
-}
+module.exports = nextConfig
