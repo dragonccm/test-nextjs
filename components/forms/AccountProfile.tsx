cd18a5ebe017f94229a92da4734831559c0cf587
dragonccm <121989450+dragonccm@users.noreply.github.com>
2023-09-06 16:35:36 +0700

    K

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ChangeEvent, use } from "react";
+import { ChangeEvent, use, useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod'
 import { UserValidation } from "@/lib/validation/user";
@@ -31,19 +31,39 @@ interface Props {
     btnTitle: string;
 }
 const AccountProfile = ({ user, btnTitle }: Props) => {
+    const [files, setFiles] = useState<File[]>([]);
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
-            profile_photo: user?.image|| "",
-            name: user?.name|| "",
-            username: user?.username|| "",
-            bio: user?.bio|| "",
+            profile_photo: user?.image || "",
+            name: user?.name || "",
+            username: user?.username || "",
+            bio: user?.bio || "",
         }
 
     });
-    const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+    const handleImage = (
+        e: ChangeEvent<HTMLInputElement>,
+        fieldChange: (value: string) => void
+    ) => {
         e.preventDefault();
-    }
+
+        const fileReader = new FileReader();
+
+        if (e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+            setFiles(Array.from(e.target.files));
+
+            if (!file.type.includes("image")) return;
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || "";
+                fieldChange(imageDataUrl);
+            };
+
+            fileReader.readAsDataURL(file);
+        }
+    };
     function onSubmit(values: z.infer<typeof UserValidation>) {
         console.log(values)
     }
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -7,7 +7,7 @@ function Topbar() {
         <nav className="topbar">
             <Link href="/" className='flex items-center gap-4'>
                 <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
-                <p className="text-heading3-bold text-light-1 max-xs:hidden">WTF Chat</p>
+                <p className="text-heading3-bold text-light-1 max-xs:hidden">KÊNh CHAT THỨ N </p>
             </Link>
             <div className='flex items-center gap-1'>
                 <div className='block md:hidden'>
