8d346f3424757b2c63910fdec2794943c7346e7a
dragonccm <121989450+dragonccm@users.noreply.github.com>
2023-09-17 15:24:16 +0700

    new file:   app/api/uploadthing/core.ts 	new file:   app/api/uploadthing/route.ts 	modified:   components/forms/AccountProfile.tsx 	new file:   lib/uploadthing.ts 	modified:   lib/utils.ts

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,6 +18,8 @@ import {
 import { Input } from "@/components/ui/input"
 import { z } from "zod";
 import { Textarea } from "../ui/textarea";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing} from "@/lib/uploadthing"
 
 interface Props {
     user: {
@@ -32,6 +34,7 @@ interface Props {
 }
 const AccountProfile = ({ user, btnTitle }: Props) => {
     const [files, setFiles] = useState<File[]>([]);
+    const {startUpload} = useUploadThing("media");
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
@@ -64,8 +67,20 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             fileReader.readAsDataURL(file);
         }
     };
-    function onSubmit(values: z.infer<typeof UserValidation>) {
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+        const blob = values.profile_photo;
+    
+        const hasImageChanged = isBase64Image(blob);
+        if (hasImageChanged) {
+          const imgRes = await startUpload(files);
+    
+          if (imgRes && imgRes[0].fileUrl) {
+            values.profile_photo = imgRes[0].fileUrl;
+          }
+        }
+    
+
+        // todo: upload user profile
     }
     return (
         <Form {...form}>
