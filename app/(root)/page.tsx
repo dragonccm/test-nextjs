d16ddc819d55cc0f5dd4ad4c708c9534410dce82
dragonccm <121989450+dragonccm@users.noreply.github.com>
2023-10-09 17:22:19 +0700

    3:43:30

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,6 @@ import { currentUser } from "@clerk/nextjs";
 export default async function Home() {
   const result = await fetchPost(1, 30);
   const user = await currentUser();
-  console.log(result);
   return (
     <div>
       <h1 className="head-text text-left ">Home</h1>
--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -3,6 +3,7 @@ import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../validation/mongoose"
+import { Console } from "console";
 
 interface Params {
   text: string,
@@ -59,4 +60,75 @@ export  async function fetchPost(pageNumber=1,pageSize=20) {
   const isNext = totalPostCount > skipAmount +posts.length;
   return { posts, isNext };
 
+}
+
+export async function fetchThreadById(threadId: string) {
+  connectToDB();
+
+  try {
+    const thread = await Thread.findById(threadId)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      }) // Populate the author field with _id and usernam
+      .populate({
+        path: "children", // Populate the children field
+        populate: [
+          {
+            path: "author", // Populate the author field within children
+            model: User,
+            select: "_id id name parentId image", // Select only _id and username fields of the author
+          },
+          {
+            path: "children", // Populate the children field within children
+            model: Thread, // The model of the nested children (assuming it's the same "Thread" model)
+            populate: {
+              path: "author", // Populate the author field within nested children
+              model: User,
+              select: "_id id name parentId image", // Select only _id and username fields of the author
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (err) {
+    console.error("Error while fetching thread:", err);
+    throw new Error("Unable to fetch thread");
+  }
+}
+
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) {
+  connectToDB();
+
+  try {
+
+    const originalThread = await Thread.findById(threadId);
+
+    if (!originalThread) {
+      throw new Error("Thread not found");
+    }
+
+
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId, 
+    });
+    const savedCommentThread = await commentThread.save();
+    originalThread.children.push(savedCommentThread._id);
+    await originalThread.save();
+
+    revalidatePath(path);
+  } catch (err) {
+    console.error("Error while adding comment:", err);
+    throw new Error("Unable to add comment");
+  }
 }
