662fa4686d6e59783b502af007d20c69aae82d94
dragonccm <121989450+dragonccm@users.noreply.github.com>
2023-10-12 08:30:49 +0700

    .

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,22 +1,28 @@
-"use server"
+"use server";
 
+import { FilterQuery, SortOrder } from "mongoose";
+import { revalidatePath } from "next/cache";
+
+import Community from "../models/community.model";
+import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../validation/mongoose";
-import { revalidatePath } from "next/cache"
+
 
 
 export async function fetchUser(userId: string) {
   try {
     connectToDB();
-    return await User.findOne({ id: userId })
-    // .populate({
-    //   path:'communities',
-    //   model: community
-    // });
+
+    return await User.findOne({ id: userId }).populate({
+      path: "communities",
+      model: Community,
+    });
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}`);
   }
 }
+
 interface Params {
   userId: string;
   username: string;
@@ -25,6 +31,7 @@ interface Params {
   image: string;
   path: string;
 }
+
 export async function updateUser({
   userId,
   bio,
@@ -39,7 +46,7 @@ export async function updateUser({
     await User.findOneAndUpdate(
       { id: userId },
       {
-        username,
+        username: username.toLowerCase(),
         name,
         bio,
         image,
@@ -52,10 +59,126 @@ export async function updateUser({
       revalidatePath(path);
     }
   } catch (error: any) {
-    console.log(error);
     throw new Error(`Failed to create/update user: ${error.message}`);
   }
 }
 
-// tao trang thai
-// export async function 
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: [
+        {
+          path: "community",
+          model: Community,
+          select: "name id image _id", 
+        },
+        {
+          path: "children",
+          model: Thread,
+          populate: {
+            path: "author",
+            model: User,
+            select: "name image id", 
+          },
+        },
+      ],
+    });
+    return threads;
+  } catch (error) {
+    console.error("Error fetching user threads:", error);
+    throw error;
+  }
+}
+
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectToDB();
+
+    
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    
+    const regex = new RegExp(searchString, "i");
+
+    
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId }, 
+    };
+
+    
+    if (searchString.trim() !== "") {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+
+    
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+
+    
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+}
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    
+    const userThreads = await Thread.find({ author: userId });
+
+    
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId }, 
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error) {
+    console.error("Error fetching replies: ", error);
+    throw error;
+  }
+}
