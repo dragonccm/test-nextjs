e992e2f6e9fc405de5586caac7fd64874e4a95e1
dragonccm <121989450+dragonccm@users.noreply.github.com>
2023-10-28 23:24:18 +0700

    .

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -59,7 +59,18 @@ const ThreadCard = ({
                             </h4>
                         </Link>
                         <p className="mt-2 text-small-regular text-light-2">{content}</p>
-                        <div className= {`${isComment && 'mb-10'} mt-5 flex flex-col gap-3`}>
+                        <div className={`${isComment && 'mb-10'} mt-5 flex flex-col gap-3`}>
+                            
+                            {/* <div className="trangbia w-30 h-50 mx-auto bg-gray-100 flex justify-center items-center">
+                                <Image
+                                    src={author.image}
+                                    alt='heart'
+                                    width={500}
+                                    height={300}
+                                    className="w-full h-full"
+                                />
+                            </div> */}
+
                             <div className="flex hap-3.5">
                                 <Image
                                     src='/assets/heart-gray.svg'
