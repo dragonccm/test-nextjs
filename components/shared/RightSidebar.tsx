34be27c785c9c343c65bcf00a8cd79f32d279855
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-03-23 16:26:02 +0700

    modified:   components/shared/RightSidebar.tsx

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -8,8 +8,6 @@ function RightSidebar() {
             <div className="flex flex-1 flex-col justify-start">
                 <h3 className="text-heading4-medium text-light-1">suggested users</h3>
             </div>
-
-
         </section>
     )
 
